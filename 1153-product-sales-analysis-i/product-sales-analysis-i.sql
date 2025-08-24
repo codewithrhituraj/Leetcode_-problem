86d3dd4431a54fb79e43cac3f3dbad7b563f67c5
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-08-24 22:38:52 +0530

    Time: 1840 ms (7.45%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -3,7 +3,7 @@
 -- from Sales 
 -- inner join Product 
 -- on Sales.product_id = Product.product_id;
-select  p.product_name , s.year ,  s.price
-from Sales s
-join Product p
-on s.product_id =p.product_id 
+select  p.product_name , s.year , s.price
+from sales s
+inner join Product p
+on s.product_id = p.product_id ;
