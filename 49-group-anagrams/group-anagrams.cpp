673c636b10b39e6b8bb6fc5dbc8b176c850b5def
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-08-31 17:04:10 +0530

    Time: 9 ms (94.94%) | Memory: 24.9 MB (76.94%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,19 +1,65 @@
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         int n=strs.size();
+//         unordered_map<string,vector<string>> mp;
+//         vector<vector<string>> res;
+//         for(int i=0;i<n;i++){
+//             string temp= strs[i];
+//             sort(begin(temp),end(temp));
+//             mp[temp].push_back(strs[i]);
+//         }
+
+//         for(auto it: mp){
+//             res.push_back(it.second);
+//         }
+//         return res;
+        
+//     }
+// };
+
+
+
+//Approach-2
+//T.C : O(n*k)  (n = size of input, k = maximum length of a string in the input vector)
+//S.C : O(n*k)
 class Solution {
 public:
+    
+    string generate(string &s) {
+        int count[26] = {0};
+        
+        for(char &ch : s) {
+            count[ch-'a']++;
+        }
+        
+        string new_s;
+        
+        for(int i = 0; i<26; i++) {
+            
+            if(count[i] > 0) {
+                new_s += string(count[i], i+'a');
+            }
+        }
+        
+        return new_s;
+    }
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n=strs.size();
-        unordered_map<string,vector<string>> mp;
-        vector<vector<string>> res;
-        for(int i=0;i<n;i++){
-            string temp= strs[i];
-            sort(begin(temp),end(temp));
-            mp[temp].push_back(strs[i]);
+        unordered_map<string, vector<string>> mp;
+        
+        for(string &s : strs) {
+            string new_s = generate(s);
+            
+            mp[new_s].push_back(s);
         }
-
-        for(auto it: mp){
-            res.push_back(it.second);
+        
+        vector<vector<string>> result;
+        for(auto &it : mp) {
+            result.push_back(std::move(it.second));
         }
-        return res;
+        
+        return result;
         
     }
 };
