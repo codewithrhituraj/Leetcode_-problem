48ce70067c89005b8e193152d8bee2b076f1d62b
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-09-01 15:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.01%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for(char& ch: s){
-            if(st.empty() or ch=='(' or ch=='{' or ch=='['){
+        for( auto& ch : s){
+            if(st.empty() or ch =='(' or ch=='{' or ch=='['){
                 st.push(ch);
                 continue;
             }
@@ -12,24 +12,25 @@ public:
                     st.pop();
                 }
                 else return false;
+
+                
             }
-
-
             else if(ch=='}'){
                 if(st.top()=='{'){
                     st.pop();
                 }
                 else return false;
             }
-
             else if(ch==']'){
                 if(st.top()=='['){
                     st.pop();
                 }
                 else return false;
             }
+            
+
         }
-        return st.empty() ? true : false;
+        return st.empty() ? true: false;
         
     }
 };
