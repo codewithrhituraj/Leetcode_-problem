93b2cf21a42dc82db511500c43bc13519c0c2ab5
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-09-03 13:39:18 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (82.01%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,14 +5,13 @@ public:
         int k=0;
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                nums[k]=nums[i];
-                k++;
+                nums[k++]=nums[i];
+                
             }
         }
-
-        for(int i=k;i<nums.size();i++){
-            nums[i]=0;
+        for(int i=k;i<n;i++){
+            nums[k++]=0;
         }
-        
+
     }
 };
