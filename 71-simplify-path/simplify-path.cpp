8338f013f94c0834eddb6e202ee7c1c3ff88494f
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-09-05 14:04:13 +0530

    Time: 1 ms (77.88%) | Memory: 15.2 MB (24.07%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -3,23 +3,26 @@ public:
     string simplifyPath(string path) {
         stack<string> st;
         stringstream ss(path);
-        string token="";
-        while(getline(ss,token,'/')){ //ss->token with the basis of /
-            if(token=="." or token==""){
-                continue;
-            }
-            if(token!=".."){
-                st.push(token);
-            }
-            else if(!st.empty() and token=="..")
-            st.pop();
+        string token;
+        while(getline(ss,token,'/')){ // tokenise the string with the basis of /
+          if(token=="" or token==".") continue;
+          if(token != ".."){
+            st.push(token);
         }
+        else if(!st.empty() and token==".."){
+            st.pop();
+         }
+
+        }
+
+
         if(st.empty()) return "/";
-        string res="";
+        string res;
         while(!st.empty()){
             res="/"+st.top()+res;
             st.pop();
         }
         return res;
+        
     }
 };
