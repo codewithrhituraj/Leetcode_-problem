8c903c819cfb1fe055bb8a57a90aed9fd593f9a0
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-10-24 21:30:02 +0530

    Time: 9 ms (13.11%) | Memory: 23.6 MB (13.20%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        // stringstream ss(s);
-        // string token="";
-        // string res="";
-        // while(ss>>token){ // ss take the words from the string and ss>> token means one words transfer to the token one a=fter one ;
-        //     res= token+" "+res;
-        // }
-        // res.pop_back();
-        // return res;
         stringstream ss(s);
-        string token="";
         string res="";
+        string token="";
         while(ss>>token){
             res=token+" "+res;
         }
         res.pop_back();
         return res;
-
-        
     }
 };
