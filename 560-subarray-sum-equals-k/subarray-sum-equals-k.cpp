cc59731cca5c5c5e9df53304230aea990b726c7a
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-10-30 22:17:55 +0530

    Time: 34 ms (91.41%) | Memory: 45.5 MB (54.50%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+       // sum,cnt;
         int n=nums.size();
+        unordered_map<int,int> mp; //sum,cnt;
+        mp.insert({0,1});
         int cnt=0;
-        unordered_map<int,int> mp; // sum,cnt;
         // 1,2,3----> 2-2 =0 ,3-2=1   mp[1]=1; beacuse we deal with prefix sum so we assume 0 has 1 value beacause in prefix sum we calculated the value before 1 
         int sum=0;
-        mp.insert({0,1});
         for(int i=0;i<n;i++){
             sum+=nums[i];
             if(mp.count(sum-k)){
                 cnt+=mp[sum-k];
-
             }
             mp[sum]++;
         }
