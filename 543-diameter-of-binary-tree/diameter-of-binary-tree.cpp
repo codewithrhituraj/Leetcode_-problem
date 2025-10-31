32f4d54a817695637898f644ec7aa6ba0124897a
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-10-31 21:20:47 +0530

    Time: 331 ms (5.08%) | Memory: 23.9 MB (21.16%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -14,12 +14,12 @@ public:
 int mx=0;
     int levels(TreeNode* root){
         if(root==NULL) return 0;
-        return 1+ max(levels(root->left), levels(root->right));
+        return 1+max(levels(root->left),levels(root->right));
     }
     int diameterOfBinaryTree(TreeNode* root) {
         if(root==NULL) return 0;
-        int dia= levels(root->left) + levels (root->right); // ye root ke perspective se sirft ek ab mujhe har ek root ke lie nikal na hoga 
-        mx=max(dia,mx);
+        int dia= levels(root->left)+levels(root->right);
+        mx=max(mx,dia);
         diameterOfBinaryTree(root->left);
         diameterOfBinaryTree(root->right);
         return mx;
