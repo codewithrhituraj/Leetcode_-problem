f98ce6af7c99860a10854271f88730a7f0918789
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-12-05 19:03:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (17.76%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,13 +4,16 @@ public:
         int n=strs.size();
         sort(begin(strs),end(strs));
         string first=strs[0];
-        string second=strs[n-1];
+        string second= strs[n-1];
         string temp="";
         for(int i=0;i<min(first.size(),second.size());i++){
-            if(first[i]==second[i])
-            temp+=first[i];
+            if(first[i]==second[i]){
+                temp+=first[i];
+
+            }
             else return temp;
-        }  
-        return temp;      
+        }
+        return temp;
+        
     }
 };
