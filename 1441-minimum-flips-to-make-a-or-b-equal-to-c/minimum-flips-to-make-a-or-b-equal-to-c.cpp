deaea9e0c041512bae35ed186d194683f6428f17
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2025-12-12 21:03:46 +0530

    Time: 3 ms (23.14%) | Memory: 7.7 MB (65.75%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -4,18 +4,16 @@ public:
         int flip=0;
         while(a!=0 or b!=0 or c!=0){
             if((c&1)==1){
-                if((a&1)==0 and (b&1)==0){
-                    flip++;
-                }
+                if((a&1)==0 and (b&1)==0) flip++;
             }
-            else{ //msb==0
-            if(a&1==1) flip++;
-            if(b&1==1) flip++;
-
+            else{
+                if((a&1)==1) flip++;
+                if((b&1)==1) flip++;
             }
             a>>=1;
             b>>=1;
             c>>=1;
+
         }
         return flip;
         
