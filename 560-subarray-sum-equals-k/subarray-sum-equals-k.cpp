2125541ccc48bffe5060a559baf2b803ec9f0c9e
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2026-01-27 15:26:33 +0530

    Time: 37 ms (78.65%) | Memory: 45.4 MB (55.27%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
+
+        // i have to store prefix sum with how many time you saw 
+
+         // (nums[i]-k) --> in stored prefix sum then there is a possibility that there is a subbarray possible 
         unordered_map<int,int> mp;
-       int sum=0;
+        mp[0]=1;
+        int sum=0;
         int cnt=0;
-        mp.insert({0,1});
         for(int i=0;i<n;i++){
             sum+=nums[i];
-            if(mp.count(sum-k)){
-                cnt+=mp[sum-k];
-            }
+            if(mp.count(sum-k)) cnt+=mp[sum-k];
             mp[sum]++;
+
         }
         return cnt;
+
         
     }
 };
