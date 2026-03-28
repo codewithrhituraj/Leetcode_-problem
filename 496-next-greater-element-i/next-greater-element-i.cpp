db79fe90d9081c5fb2faa023336066aa11dee603
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2026-03-28 18:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (13.57%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,21 +4,25 @@ public:
         int n= nums2.size();
         unordered_map<int,int> mp;
         stack<int> st;
-        for(int i= n-1;i>=0;i--){
-            while(st.size()>0 and st.top() <= nums2[i]){
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() and st.top() <=nums2[i]){
                 st.pop();
             }
-
             if(st.size()==0) mp[nums2[i]]=-1;
             else {
                 mp[nums2[i]]=st.top();
+
             }
+
+
+           // mp[nums2[i]]=-1;
             st.push(nums2[i]);
         }
         vector<int> ans;
-        for(int & v: nums1){
-            ans.push_back(mp[v]);
 
+        for(auto& c: nums1){
+            ans.push_back(mp[c]);
+            
         }
         return ans;
         
