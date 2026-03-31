e5827e8c69bad0f2f5216c6aec05bcba3c9d5db4
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2026-03-31 19:50:10 +0530

    Time: 49 ms (26.33%) | Memory: 75.9 MB (6.43%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,42 +10,48 @@
  */
 class Solution {
 public:
-    ListNode* Merge_Sort(ListNode* a,ListNode* b){
+    ListNode* merge(ListNode* l1, ListNode* l2){
         ListNode* dummy = new ListNode(-1);
-        ListNode* temp= dummy;
-        while(a!=NULL and b!=NULL){
-            if(a->val<b->val){
-                temp->next=a;
+        ListNode* temp=dummy;
+        while(l1!=NULL and l2!=NULL){
+            if(l1->val<l2->val){
+                temp->next=l1;
+                l1=l1->next;
                 temp=temp->next;
-                a=a->next;
             }
             else{
-                temp->next=b;
+                temp->next=l2;
+                l2=l2->next;
                 temp=temp->next;
-                b=b->next;
             }
         }
-        if(a==NULL) temp->next=b;
-        else temp->next= a;
+        if(l1==NULL) temp->next=l2;
+        else temp->next=l1;
+        
 
         return dummy->next;
+
+
     }
     ListNode* sortList(ListNode* head) {
         if(head==NULL or head->next==NULL) return head;
-        ListNode* fast= head;
         ListNode* slow= head;
-        while(fast->next!=NULL and fast->next->next!=NULL){//for left middle 
+        ListNode* fast= head;
+        while(fast->next!=NULL and fast->next->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
         }
-        ListNode* a=head;
+
+       // slow->next=NULL;
+        ListNode* a= head;
         ListNode* b= slow->next;
         slow->next=NULL;
-        // you have to call sortlist function for updating list from one 
+
         a=sortList(a);
         b=sortList(b);
-        ListNode* c= Merge_Sort(a,b);
-        return c;
+        return merge(a,b);
+
+
         
     }
 };
