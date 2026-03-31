f99eadd9b8e9be20cd1dca424ec64ae527cb520e
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2026-03-31 20:09:36 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (73.59%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -11,28 +11,25 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode* temp=head;
+        ListNode* temp = head;
         ListNode* lo= new ListNode(-1);
-        ListNode* hi= new ListNode(1);
+        ListNode* hi= new ListNode(-2);
         ListNode* templo=lo;
-        ListNode* temphi=hi;
+        ListNode* temphi= hi;
         while(temp!=NULL){
             if(temp->val<x){
                 templo->next=temp;
-                temp=temp->next;
                 templo=templo->next;
+                temp=temp->next;
             }
             else{
                 temphi->next=temp;
-                temp=temp->next;
                 temphi=temphi->next;
+                temp=temp->next;
             }
         }
         templo->next=hi->next;
         temphi->next=NULL;
         return lo->next;
-
-
-        
     }
 };
