ad679a274ee50684df89c2f5922e0c4d56099ad1
codewithrhituraj <145393460+codewithrhituraj@users.noreply.github.com>
2026-05-10 12:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (88.13%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,26 +3,34 @@ public:
     bool isValid(string s) {
         stack<char> st;
         int n= s.length();
-        for(auto& x: s){
-            if(st.empty() or x=='(' or x=='{' or x=='['){
-                st.push(x);
+        for(int i=0;i<n;i++){
+            if(st.empty() or s[i]=='(' or s[i]=='{' or s[i]=='[' ){
+                st.push(s[i]);
                 continue;
             }
-            else if(x==')'){
-                if(st.top()=='(') st.pop();
+            else if(s[i]==')'){
+                if(st.top()=='('){
+                    st.pop();
+                }
                 else return false;
-
             }
-            else if(x=='}'){
-                if(st.top()=='{') st.pop();
-                else return false;
+            else if(s[i]=='}'){
+                if(st.top()=='{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
             }
             else{
-                if(st.top()=='[') st.pop();
+                if(st.top()=='['){
+                    st.pop();
+                }
                 else return false;
             }
         }
-        return st.empty()? true: false;
+        if(st.empty()) return true;
+        else return false;
         
     }
 };
